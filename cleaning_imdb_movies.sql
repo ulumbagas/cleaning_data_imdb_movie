f3bcda1516a13918fcf8851235b45aaee4a3163e
ulumbagas <ulumbagas@gmail.com>
2023-07-23 22:46:49 +0700

    finishing cleaning data

--- cleaning_imdb_movies.sql
+++ cleaning_imdb_movies.sql
@@ -1,12 +1,17 @@
 select * from imdb_movies;
 
-select distinct(Rating) from imdb_movies
-order by Rating;
+SELECT DISTINCT
+    (Rating)
+FROM
+    imdb_movies
+ORDER BY Rating;
 
 #update blank to nan not a number
-update imdb_movies set
-Rating = 'nan'
-where Rating = '';
+UPDATE imdb_movies 
+SET 
+    Rating = 'nan'
+WHERE
+    Rating = '';
 
 select distinct(Year) from imdb_movies
 order by Year;
@@ -48,5 +53,71 @@ update imdb_movies set
 Filming_location = 'United Kingdom'
 where Filming_location = 'UK';
 
-/*ALTER TABLE imdb_movies
-ADD COLUMN Budget_Copy TEXT;*/
+select Distinct(Left(Budget,1)) from imdb_movies;
+
+select Budget_copy from imdb_movies;
+
+select Budget_copy from imdb_movies
+where Budget like '%C%' Or
+Budget like '%S%' Or
+Budget like '%D%' Or
+Budget like '%A%' Or
+Budget like '%N%' And
+Budget not like '%Unknown%' ;
+
+
+ALTER TABLE imdb_movies
+ADD COLUMN Budget_Copy TEXT;
+
+update imdb_movies set  Budget_copy = Budget;
+
+update imdb_movies set  Budget_copy = trim(Budget_copy);
+
+update imdb_movies set  Budget_copy = replace(Budget_copy,',','');
+
+update imdb_movies set  Budget_copy = replace(Budget_copy,' ','');
+
+update imdb_movies set  Budget_copy = replace(Budget_copy,'Unknown','0');
+
+update imdb_movies set  Budget_copy = 
+Case 
+when left(Budget_copy,3)= 'CA$' then replace(Budget_copy,'CA$','')
+when left(Budget_copy,3)= 'SEK' then replace(Budget_copy,'SEK','')
+when left(Budget_copy,3)= 'DKK' then replace(Budget_copy,'DKK','')
+when left(Budget_copy,2)= 'A$' then replace(Budget_copy,'A$','')
+when left(Budget_copy,3)= 'NOK' then replace(Budget_copy,'NOK','')
+when left(Budget_copy,3)= 'CN¥' then replace(Budget_copy,'CN¥','')
+when left(Budget_copy,1)= '€' then replace(Budget_copy,'€','')
+when left(Budget_copy,1)= '₹' then replace(Budget_copy,'₹','')
+when left(Budget_copy,1)= '£' then replace(Budget_copy,'£','')
+when left(Budget_copy,1)= '₩' then replace(Budget_copy,'₩','')
+when left(Budget_copy,1)= '¥' then replace(Budget_copy,'¥','')
+when left(Budget_copy,1)= '$' then replace(Budget_copy,'$','')
+else Budget_copy end;
+
+ALTER TABLE `belajar_gan`.`imdb_movies` 
+CHANGE COLUMN `Budget_Copy` `Budget_Copy` BIGINT NULL DEFAULT NULL ;
+
+select Budget,Budget_copy from imdb_movies
+where Budget not like '%$%' and Budget != 'Unknown';
+
+#cleaning income
+select Income from imdb_movies
+where Income != '0';
+
+update imdb_movies set
+income = '0'
+where income = 'Unknown';
+
+update imdb_movies set
+income = trim(income);
+
+update imdb_movies set
+income = replace(income,',','');
+
+update imdb_movies set
+income = replace(income,'$','')
+where income like '%$%';
+
+ALTER TABLE `belajar_gan`.`imdb_movies` 
+CHANGE COLUMN `Income` `Income` BIGINT NULL DEFAULT NULL;
