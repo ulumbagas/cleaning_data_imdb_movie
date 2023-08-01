bb44e8600b1d371e26fb194d83858cec0e1eed0b
ulumbagas <ulumbagas@gmail.com>
2023-08-01 21:57:06 +0700

    optimize all code

--- cleaning_imdb_movies.sql
+++ cleaning_imdb_movies.sql
@@ -1,10 +1,11 @@
 select * from imdb_movies;
 
-SELECT DISTINCT
-    (Rating)
+SELECT distinct(Rating)
 FROM
     imdb_movies
 ORDER BY Rating;
+
+select avg(Rating_copy1) from imdb_movies;
 
 #update blank to nan not a number
 UPDATE imdb_movies 
@@ -16,48 +17,40 @@ WHERE
 select distinct(Year) from imdb_movies
 order by Year;
 
-select distinct(Month) from imdb_movies
-order by Month;
+select distinct(`Month`) from imdb_movies;
 
 #update 2008 and 2014 to unknown
-update imdb_movies set
-Month = 'unknown'
-where Month in ('2008','2014');
+
+UPDATE imdb_movies 
+SET 
+    Month = 'unknown'
+WHERE
+    Month IN ('2008' , '2014');
 
 #update blank and not rated to unrated
 update imdb_movies set
 Certificate = 'unrated'
 where Certificate in ('not rated','');
 
-select distinct(Runtime) from imdb_movies
-order by Runtime;
-
-select distinct(Stars) from imdb_movies
-order by Stars;
-
-select distinct(Genre) from imdb_movies
-order by Genre;
 
 select distinct(Filming_location) from imdb_movies
 order by Filming_location; #'The Netherlands' facebook  UK 'United Kingdom'
 
-update imdb_movies set
-Filming_location = 'Netherlands'
-where Filming_location = 'The Netherlands';
-
-update imdb_movies set
-Filming_location = 'unknown'
-where Filming_location = 'Official Facebook';
-
-update imdb_movies set
-Filming_location = 'United Kingdom'
-where Filming_location = 'UK';
+UPDATE imdb_movies
+SET
+    Filming_location = 
+    CASE 
+        WHEN Filming_location = 'The Netherlands' THEN 'Netherlands'
+        WHEN Filming_location = 'Official Facebook' THEN 'unknown'
+        WHEN Filming_location = 'UK' THEN 'United Kingdom'
+        ELSE Filming_location
+    END;
 
 select Distinct(Left(Budget,1)) from imdb_movies;
 
-select Budget_copy from imdb_movies;
+select Budget_USD from imdb_movies;
 
-select Budget_copy from imdb_movies
+select Budget from imdb_movies
 where Budget like '%C%' Or
 Budget like '%S%' Or
 Budget like '%D%' Or
@@ -67,38 +60,38 @@ Budget not like '%Unknown%' ;
 
 
 ALTER TABLE imdb_movies
-ADD COLUMN Budget_Copy TEXT;
+ADD COLUMN Budget_USD TEXT;
 
-update imdb_movies set  Budget_copy = Budget;
+update imdb_movies set  Budget_USD = Budget;
 
-update imdb_movies set  Budget_copy = trim(Budget_copy);
+update imdb_movies set  Budget_USD = trim(Budget_USD);
 
-update imdb_movies set  Budget_copy = replace(Budget_copy,',','');
+UPDATE imdb_movies
+SET Budget_USD = CASE
+	WHEN Budget_USD = 'Unknown' THEN 0
+    ELSE REPLACE(Budget_USD, ',', '')
+END;
 
-update imdb_movies set  Budget_copy = replace(Budget_copy,' ','');
-
-update imdb_movies set  Budget_copy = replace(Budget_copy,'Unknown','0');
-
-update imdb_movies set  Budget_copy = 
+update imdb_movies set  Budget_USD = 
 Case 
-when left(Budget_copy,3)= 'CA$' then replace(Budget_copy,'CA$','')
-when left(Budget_copy,3)= 'SEK' then replace(Budget_copy,'SEK','')
-when left(Budget_copy,3)= 'DKK' then replace(Budget_copy,'DKK','')
-when left(Budget_copy,2)= 'A$' then replace(Budget_copy,'A$','')
-when left(Budget_copy,3)= 'NOK' then replace(Budget_copy,'NOK','')
-when left(Budget_copy,3)= 'CN¥' then replace(Budget_copy,'CN¥','')
-when left(Budget_copy,1)= '€' then replace(Budget_copy,'€','')
-when left(Budget_copy,1)= '₹' then replace(Budget_copy,'₹','')
-when left(Budget_copy,1)= '£' then replace(Budget_copy,'£','')
-when left(Budget_copy,1)= '₩' then replace(Budget_copy,'₩','')
-when left(Budget_copy,1)= '¥' then replace(Budget_copy,'¥','')
-when left(Budget_copy,1)= '$' then replace(Budget_copy,'$','')
-else Budget_copy end;
+when left(Budget_USD,3)= 'CA$' then replace(Budget_USD,'CA$','')
+when left(Budget_USD,3)= 'SEK' then replace(Budget_USD,'SEK','')
+when left(Budget_USD,3)= 'DKK' then replace(Budget_USD,'DKK','')
+when left(Budget_USD,2)= 'A$' then replace(Budget_USD,'A$','')
+when left(Budget_USD,3)= 'NOK' then replace(Budget_USD,'NOK','')
+when left(Budget_USD,3)= 'CN¥' then replace(Budget_USD,'CN¥','')
+when left(Budget_USD,1)= '€' then replace(Budget_USD,'€','')
+when left(Budget_USD,1)= '₹' then replace(Budget_USD,'₹','')
+when left(Budget_USD,1)= '£' then replace(Budget_USD,'£','')
+when left(Budget_USD,1)= '₩' then replace(Budget_USD,'₩','')
+when left(Budget_USD,1)= '¥' then replace(Budget_USD,'¥','')
+when left(Budget_USD,1)= '$' then replace(Budget_USD,'$','')
+else Budget_USD end;
 
-ALTER TABLE `belajar_gan`.`imdb_movies` 
-CHANGE COLUMN `Budget_Copy` `Budget_Copy` BIGINT NULL DEFAULT NULL ;
+ALTER TABLE `cleaning_table`.`imdb_movies` 
+CHANGE COLUMN `Budget_USD` `Budget_USD` BIGINT NULL DEFAULT NULL ;
 
-select Budget,Budget_copy from imdb_movies
+select Budget,Budget_USD from imdb_movies
 where Budget not like '%$%' and Budget != 'Unknown';
 
 #cleaning income
@@ -119,5 +112,5 @@ update imdb_movies set
 income = replace(income,'$','')
 where income like '%$%';
 
-ALTER TABLE `belajar_gan`.`imdb_movies` 
+ALTER TABLE `cleaning_table`.`imdb_movies` 
 CHANGE COLUMN `Income` `Income` BIGINT NULL DEFAULT NULL;
